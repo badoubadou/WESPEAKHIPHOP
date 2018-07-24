bdff8ab15bf421b635fc67b94769fef5451648a0
Romain Malauzat <rmalauzat@gmail.com>
2018-07-24 17:15:08 +0200

    test pug

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,43 @@ var stylus = require('gulp-stylus');
 var pug    = require('gulp-pug');
 
 gulp.task('javascript', function() {
-  return gulp.src('assets/javascript/*.js')
-    .pipe(wrap('(function($, window){<%= contents %>}(jQuery, window));'))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/javascripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+	return gulp.src('assets/javascript/*.js')
+		.pipe(wrap('(function($, window){<%= contents %>}(jQuery, window));'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/javascripts'))
+		.pipe(notify({ message: 'Scripts task complete' }));
 });
 
 gulp.task('css', function() {
-  return gulp.src('assets/style/*.styl')
-    .pipe(stylus({
-      use: nib(),
-      compress: true
-    }))
-    .pipe(gulp.dest('public/stylesheets'))
-    .pipe(notify({ message: 'CSS task complete' }));
+	return gulp.src('assets/style/*.styl')
+		.pipe(stylus({
+			use: nib(),
+			compress: true
+		}))
+		.pipe(gulp.dest('public/stylesheets'))
+		.pipe(notify({ message: 'CSS task complete' }));
 });
  
 gulp.task('pug', function buildHTML() {
-  return gulp.src('assets/pug/*.pug')
-  .pipe(pug({
-    // Your options in here.
-  }))
-  .pipe(gulp.dest('public/'))
+	return gulp.src('assets/pug/*.pug')
+		.pipe(pug({
+		// Your options in here.
+		}))
+		.pipe(gulp.dest('public/'))
 });
 
 gulp.task('default', function() {
-  gulp.start('javascript');
-  gulp.start('pug');
+  	gulp.start('javascript');
+  	gulp.start('pug');
 	gulp.start('css');
+	gulp.start('watch');
 });
 
 gulp.task('watch', function() {
-  gulp.watch('assets/javascript/*.js', ['javascript']);
+	gulp.watch('assets/javascript/*.js', ['javascript']);
 	gulp.watch('assets/style/*.styl', ['css']);
+	gulp.watch('assets/pug/*.pug', ['pug']);
 });
