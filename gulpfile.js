d27ab806c94b5c86116ce8e3767fbb0ec6edf1fa
Romain Malauzat <rmalauzat@gmail.com>
2018-07-26 15:51:51 +0200

    add wp xml

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,11 @@ var stylus = require('gulp-stylus');
 var pug    = require('gulp-pug');
 var runSequence = require('run-sequence');
 
+var data = require('gulp-data');
+fs = require('fs'),
+path = require('path'),
+merge = require('gulp-merge-json');
+
 var gutil        = require('gulp-util');
 var coffee       = require('gulp-coffee');
 var concat       = require('gulp-concat');
@@ -61,13 +66,54 @@ gulp.task('css', function() {
 		.pipe(notify({ message: 'CSS task complete' }));
 });
  
-gulp.task('pug', function buildHTML() {
-	return gulp.src(pugfile)
-		.pipe(pug({
-		// Your options in here.
-		}))
-		.pipe(gulp.dest(dest))
+// gulp.task('pug', function buildHTML() {
+// 	return gulp.src(pugfile)
+// 		.pipe(pug({
+// 		// Your options in here.
+// 		}))
+// 		.pipe(gulp.dest(dest))
+// });
+
+// gulp.task('pug', function() {
+//     return gulp.src(pugfile)
+//         .pipe(data(function(file) {
+//             return JSON.parse(fs.readFileSync('assets/temp/data.json'))
+//         }))
+//         .pipe(pug())
+//         .pipe(gulp.dest(dest));
+// });
+
+gulp.task('pug:data', function() {
+    return gulp.src('assets/json/**/*.json')
+        .pipe(merge({
+            fileName: 'data.json',
+            edit: (json, file) => {
+                // Extract the filename and strip the extension
+                var filename = path.basename(file.path),
+                    primaryKey = filename.replace(path.extname(filename), '');
+
+                // Set the filename as the primary key for our JSON data
+                var data = {};
+                data[primaryKey.toUpperCase()] = json;
+
+                return data;
+            }
+        }))
+        .pipe(gulp.dest('assets/temp'));
 });
+
+gulp.task('pug', ['pug:data'], function() {
+    return gulp.src(pugfile)
+        .pipe(data(function() {
+            return JSON.parse(fs.readFileSync('assets/xml/wsh.json'))
+        }))
+        .pipe(pug({
+            pretty: true,
+            basedir: './'
+        }))
+        .pipe(gulp.dest(dest));
+});
+
 
 gulp.task('misc', function () {
 	gulp.src(misk)
@@ -89,5 +135,5 @@ gulp.task('default', ['misc'], function (cb) {
 gulp.task('watch', function() {
 	gulp.watch(jsfile, ['coffee']);
 	gulp.watch(stylfile, ['css']);
-	gulp.watch(pugfile, ['pug']);
+	gulp.watch(['assets/pug/*.pug', 'assets/module/**/*.pug'], ['pug']);
 });
