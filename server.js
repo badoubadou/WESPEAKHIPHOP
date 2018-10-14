a3b9692e586bde47186a022b516026fb9b01d17c
Romain Malauzat <rmalauzat@gmail.com>
2018-10-14 19:06:30 +0200

    header html

--- server.js
+++ server.js
@@ -72,13 +72,13 @@ http.createServer(function (request, response) {
             // Note: this is not a conformant accept-encoding parser.
             // See http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3
             if (acceptEncoding.match(/\bdeflate\b/)) {
-                response.writeHead(200, { 'content-encoding': 'deflate' });
+                response.writeHead(200, { 'content-encoding': 'deflate', 'Content-Type': contentType });
                 raw.pipe(zlib.createDeflate()).pipe(response);
             } else if (acceptEncoding.match(/\bgzip\b/)) {
-                response.writeHead(200, { 'content-encoding': 'gzip' });
+                response.writeHead(200, { 'content-encoding': 'gzip' ,  'Content-Type': contentType });
                 raw.pipe(zlib.createGzip()).pipe(response);
             } else {
-                response.writeHead(200, {});
+                response.writeHead(200, { 'Content-Type': contentType });
                 raw.pipe(response);
             }
             // response.writeHead(200, { 'Content-Type': contentType });
