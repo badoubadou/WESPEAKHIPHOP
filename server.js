87b6e531ad912f9436322b4a226e895ff724831e
Romain Malauzat <rmalauzat@gmail.com>
2018-10-17 11:03:18 +0200

    seo cache img

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function(req, res) {
     var urlStr = 'http://' + req.headers.host + req.url,
     parsedURL = url.parse( urlStr ,true );
     var filePath = '.' + req.url;
+    var extname = path.extname(filePath);
+
+    if((extname=='.jpg') || (extname=='.png') )
+        res.setHeader("Cache-Control", "public, max-age=2592000");
+        res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+
     if (filePath == './')
         if ((req.headers.host == 'www.wespeakhiphop.com') || (req.headers.host == 'wespeakhiphop.com'))
         filePath = '/public/index_en.html';
